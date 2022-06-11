cd2d330f988017567802e15c6b60da5839eb7640
NingBo Wang <2536935847@qq.com>
2022-06-11 23:10:36 +0800

    update

--- packages/react-app/src/constants.js
+++ packages/react-app/src/constants.js
@@ -10,8 +10,8 @@ export const BLOCKNATIVE_DAPPID = "0b58206a-f3c0-4701-a62f-73c7243e8c77";
 export const ALCHEMY_KEY = "oKxs-03sij-U_N0iOlrSsZFr29-IqbuF";
 
 export const NETWORKS = {
-  localhost: {
-    name: "localhost",
+  Vecechain: {
+    name: "Vecechain",
     color: "#666666",
     chainId: 300,
     blockExplorer: "",
