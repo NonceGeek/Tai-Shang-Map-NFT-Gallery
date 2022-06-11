19fe8689221ab2c7098658bb356fb2958893b0b4
99kies <1290017556@qq.com>
2022-06-11 19:17:08 +0800

    update

--- packages/react-app/scripts/ipfs.js
+++ packages/react-app/scripts/ipfs.js
@@ -1,14 +1,24 @@
-const ipfsAPI = require("ipfs-http-client");
+const {create, globSource} = require("ipfs-http-client");
 const chalk = require("chalk");
 const { clearLine } = require("readline");
-
-const { globSource } = ipfsAPI;
 
 const infura = { host: "ipfs.infura.io", port: "5001", protocol: "https" };
 // run your own ipfs daemon: https://docs.ipfs.io/how-to/command-line-quick-start/#install-ipfs
 // const localhost = { host: "localhost", port: "5001", protocol: "http" };
 
-const ipfs = ipfsAPI(infura);
+const ipfs =  create(infura);
+
+// const ipfsAPI = require("ipfs-http-client");
+// const chalk = require("chalk");
+// const { clearLine } = require("readline");
+
+// const { globSource } = ipfsAPI;
+
+// const infura = { host: "ipfs.infura.io", port: "5001", protocol: "https" };
+// run your own ipfs daemon: https://docs.ipfs.io/how-to/command-line-quick-start/#install-ipfs
+// const localhost = { host: "localhost", port: "5001", protocol: "http" };
+
+// const ipfs = ipfsAPI(infura);
 
 const ipfsGateway = "https://ipfs.io/ipfs/";
 const ipnsGateway = "https://ipfs.io/ipns/";
